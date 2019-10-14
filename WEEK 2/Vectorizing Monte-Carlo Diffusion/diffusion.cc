fa36b3297855770cc4728b51c3393d77b3d8638e
pateldevang <devangpatel92@gmail.com>
2019-10-14 14:46:54 +0530

    WEEK2 added.

---
+++ WEEK 2/Vectorizing Monte-Carlo Diffusion/diffusion.cc
@@ -0,0 +1,26 @@
+#include <mkl.h>
+#include "distribution.h"
+
+int i,j;
+
+//vectorize this function based on instruction on the lab page
+int diffusion(const int n_particles, 
+              const int n_steps, 
+              const float x_threshold,
+              const float alpha, 
+              VSLStreamStatePtr rnStream) {
+  int n_escaped=0;
+  int pos[n_particles];
+  for (int i = 0; i < n_steps; i++) {
+	  float rn[n_particles];
+    
+      //Intel MKL function to generate random numbers
+      vsRngUniform(VSL_RNG_METHOD_UNIFORM_STD, rnStream, 1, rn, -1.0, 1.0);
+      
+    for (int j = 0; j < n_particles; j++) {
+      pos[j] += dist_func(alpha, rn[j]);
+    }
+    if (pos[j] > x_threshold) n_escaped++;
+  }
+  return n_escaped;
+}

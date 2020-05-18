670f799c14f717f8a0d35ce999e53d3816f4e0dc
prashant1pandey <59042734+prashant1pandey@users.noreply.github.com>
2020-05-18 16:45:50 +0530

    diffusn.cc
    no

--- WEEK 2/Vectorizing Monte-Carlo Diffusion/diffusion.cc
+++ WEEK 2/Vectorizing Monte-Carlo Diffusion/diffusion.cc
@@ -3,7 +3,6 @@
 
 int i,j;
 
-//vectorize this function based on instruction on the lab page
 int diffusion(const int n_particles, 
               const int n_steps, 
               const float x_threshold,
@@ -14,7 +13,6 @@ int diffusion(const int n_particles,
   for (int i = 0; i < n_steps; i++) {
 	  float rn[n_particles];
     
-      //Intel MKL function to generate random numbers
       vsRngUniform(VSL_RNG_METHOD_UNIFORM_STD, rnStream, 1, rn, -1.0, 1.0);
       
     for (int j = 0; j < n_particles; j++) {
